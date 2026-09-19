41191509c07be8023fd594e8fcfd4aed4019546c
aminoqr <aminoqr2@gmail.com>
2026-09-19 14:36:18 +0200

    mariadb container

---
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+DB_PASSWORD=$(cat /run/secrets/db_password)
+DB_ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
+
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+    echo "First run: setting up the database..."
+
+    mariadb-install-db --user=mysql --datadir=/var/lib/mysql --auth-root-authentication-method=normal > /dev/null
+    
+    service mariadb start
+
+    mysql -u root <<-EOSQL
+        ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';
+        CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
+        CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
+        GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
+        FLUSH PRIVILEGES;
+EOSQL
+
+    service mariadb stop
+fi
+
+exec mariadbd --user=mysql      
